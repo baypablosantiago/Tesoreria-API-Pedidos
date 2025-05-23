51c90c36aa42db5ad360f4a2d937d13ccb0d709f
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-05-23 09:05:44 -0300

    annotations in model

--- Models/FundingRequest.cs
+++ Models/FundingRequest.cs
@@ -1,26 +1,56 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace API_Pedidos.Models;
 
 public class FundingRequest
 {
-    public long Id { get; set; }
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+    public long Id { get; set; } 
 
+
+    [Required]
     public int DA { get; set; }
 
-    public string RequestNumber { get; set; } = string.Empty;
 
+    [Required]
+    public int RequestNumber { get; set; } 
+
+
+    [Required]
     public int FiscalYear { get; set; }
 
-    public string PaymentOrderNumber { get; set; } = string.Empty;
 
+    [Required]
+    public int PaymentOrderNumber { get; set; } 
+
+
+    [Required]
+    [MaxLength(500)]
     public string Concept { get; set; } = string.Empty;
 
+
+    [Required]
+    [MaxLength(20)]
     public string DueDate { get; set; } = string.Empty;
 
+
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
     public decimal Amount { get; set; }
 
+
+    [Required]
+    [MaxLength(20)]
     public string FundingSource { get; set; } = string.Empty;
 
+
+    [Required]
+    [MaxLength(50)]
     public string CheckingAccount { get; set; } = string.Empty;
 
+
+    [MaxLength(500)]
     public string? Comments { get; set; }
 }

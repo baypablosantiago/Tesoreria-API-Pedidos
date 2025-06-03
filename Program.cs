cdd4374521420e2985ee3e274265773923814f17
Bay Pablo <bay.pablo.santiago@gmail.com>
2025-06-03 11:47:28 -0300

    cors

--- Program.cs
+++ Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("http://localhost:4200")
+            .WithOrigins("*")
             .AllowAnyHeader()
             .AllowAnyMethod();
     });
